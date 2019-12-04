ec4ede8277a2a8e3e0388d2f704713ac4e669434
Hasan Karlı <karlihasann@gmail.com>
2019-12-04 23:53:15 +0300

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:async';
 
 void main() async{
 
-  Map data = await getAllData(); 
+  Map data = await getAllData();
   print("Result is:"+data['city']['coord']['lat'].toString());
 
   runApp(new MaterialApp(
@@ -13,6 +13,43 @@ void main() async{
       appBar: AppBar(
         title: new Text("Weather Application"),
         backgroundColor: Colors.yellow,
+      ),
+      body: new Container(
+        height: 800.0,
+        child: new Stack(
+          children: <Widget>[
+            Container(
+              height: 800.0,
+              width: double.infinity,
+              child: new Image.network("https://images.unsplash.com/photo-1575439321979-bef46fb3ca8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80"
+              ,fit: BoxFit.cover,)
+            ),
+            Positioned(
+              bottom: 50.0,
+              child: Container(
+                height: 150.0,
+                width: 400.0,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.cyanAccent,
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: <Widget>[
+                     new Text(data['list'][0]['main']['temp'].toString()+"/F",
+                     style: TextStyle(color: Colors.white,
+                     fontSize: 25.0),),
+
+                     new SizedBox(height: 10.0,),
+                     new Text(data['list'][0]['weather'][0]['main'],
+                       style: TextStyle(color: Colors.white,
+                           fontSize: 25.0),)
+
+                   ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     )
   ));
