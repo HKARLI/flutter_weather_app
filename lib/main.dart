8007443b3d46d5ad7ff98f21328bae66af216bbc
Hasan Karlı <karlihasann@gmail.com>
2019-12-04 23:29:07 +0300

    add http connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
-void main(){
+void main() async{
+
+  Map data = await getAllData(); 
+  print("Result is:"+data['city']['coord']['lat'].toString());
+
   runApp(new MaterialApp(
     home: Scaffold(
       appBar: AppBar(
@@ -9,4 +16,13 @@ void main(){
       ),
     )
   ));
+}
+
+Future<Map> getAllData() async {
+  var api = "https://samples.openweathermap.org/data/2.5/forecast/hourly?id=524901&appid=b6907d289e10d714a6e88b30761fae22";
+  var data = await http.get(api);
+  var jsonData = json.decode(data.body);
+
+  return jsonData;
+
 }
