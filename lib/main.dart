9b5d7a629d6019ee313802d6f154038b89026773
Hasan Karlı <karlihasann@gmail.com>
2019-12-04 09:21:01 +0300

    new weather_app

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: new Text("Weather Application"),
+        backgroundColor: Colors.yellow,
+      ),
+    )
+  ));
+}
